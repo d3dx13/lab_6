20a73443fa4f367e845868da1cd6297cea9333d4
AlexandrNikolaev0 <roketka1@gmail.com>
2019-04-23 21:06:46 +0300

    Добавление CommandHandler (начало реализации)

--- src/lab_6/server/core/CommandHandler.java
+++ src/lab_6/server/core/CommandHandler.java
@@ -1,59 +1,16 @@
 package lab_6.server.core;
 
 import lab_6.message.Message;
+
+
 import java.util.concurrent.ConcurrentHashMap;
 
+import static lab_6.server.Database.collection;
+
 public class CommandHandler {
-    public static void main(String[] args)
+    public static Message startCommand(Message messageInThread)
     {
-        Message messageInThread = new Message();//приходит на вход функции
-        Message answer = new Message();
-        String command = messageInThread.text.trim();
-        ConcurrentHashMap<String,Object> generalCollection = new ConcurrentHashMap<>();//приходит на вход функции
-
-        String[] acceptedCommands = {"insert", "show", "remove_lower", "clear", "sort", "info", "remove", "exit"};
-
-
-        String stringOfCommands="Введена неверная команда.\nСписок доступных команд:\n remove_lower {String key}\n " +
-                "remove_lower {element}\n remove {String key}\n insert {String key} {element}\n show\n clear\n info\n sort\n exit";
-        if(command.equals("")) {
-            //сообщить о том, что введенной команды не существует
-        } else {
-            switch (command) {
-                case "info":
-                    answer.text = "Тип хранимой коллекции:"+generalCollection.getClass()
-                            +".\nКоличество элементов в коллекции:"+generalCollection.size()+".";
-                    break;
-                case "show":
-                    //commandShow(world);
-                    break;
-                case "clear":
-                    generalCollection.clear();
-                    answer.text = "Все элементы коллекции удалены.";
-                    break;
-                case "insert":
-
-
-                    //commandInsert(consoleLine, world);
-                    break;
-                case "remove":
-                    //commandRemove(consoleLine, world);
-                    break;
-                case "remove_lower":
-                    //commandRemoveLower(consoleLine, world);
-                    break;
-                case "sort":
-                    //commandSort(world);
-                    break;
-                case "exit":
-                    //save(world);
-                    break;
-                default:
-                    //System.out.println(stringOfCommands);
-                    break;
-            }
-
-        }
+       return  null;
 
 
     }
