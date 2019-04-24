d45229433efb4e26a84329393cb9077ab7c51332
AlexandrNikolaev0 <roketka1@gmail.com>
2019-04-24 13:04:13 +0300

    toString with nulls

--- src/lab_6/message/CollectionInfo.java
+++ src/lab_6/message/CollectionInfo.java
@@ -9,15 +9,30 @@ public class CollectionInfo implements Serializable {
     @Override
     public String toString()
     {
-        return new StringBuffer()
-                .append("\n--- Collection info ---")
-                .append("\nCollection type: ")
-                .append(type)
-                .append("\nLast change time: ")
-                .append(lastChangeTime)
-                .append("\nSize of collection: ")
-                .append(size)
+        if(type==null) {
+            return new StringBuffer().append("\n--- Collection info ---")
+                .append("\nCollection has null")
                 .append("\n-----------------------\n")
                 .toString();
+        }
+        if(lastChangeTime==0){
+            return new StringBuffer().append("\n--- Collection info ---")
+                .append("\nCollection has never been changed")
+                .append("\n-----------------------\n")
+                .toString();
+        }
+
+        if(type!=null&&lastChangeTime!=0) {
+            return new StringBuffer()
+                    .append("\n--- Collection info ---")
+                    .append("\nCollection type: ")
+                    .append(type)
+                    .append("\nLast change time: ")
+                    .append(lastChangeTime)
+                    .append("\nSize of collection: ")
+                    .append(size)
+                    .append("\n-----------------------\n")
+                    .toString();
+        }
     }
 }
